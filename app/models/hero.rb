2d754133f110f1b4772ca61303def6f9c0575390
YSongYS <songyishu.yang@gmail.com>
2019-01-28 17:54:12 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,20 @@
+class Hero
+  attr_accessor :name, :power, :biography
+  HEROS = []
+
+  def initialize (args)
+    @name = args[:name]
+    @power = args[:power]
+    @biography = args[:biography]
+    HEROS << self
+  end
+
+  def self.all
+    HEROS
+  end
+
+  def self.clear
+    HEROS.clear
+  end
+
+end
